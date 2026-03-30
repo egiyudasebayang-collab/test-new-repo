b2b9dff8d958bfb43446f9eb854d15063d1dd4e0
Egi Yuda <egiyudasebayang@gmail.com>
2026-03-30 16:27:54 +0700

    nambah file baru C

---
+++ test2.c
@@ -0,0 +1,16 @@
+//Program kasir sederhana
+
+#include <stdio.h>
+
+int main(){ 
+    //deklarasi variabel
+
+    //input
+    printf("=== Program Kasir Sederhana ===");
+
+    //proses
+
+    //output
+
+    return 0;
+}
