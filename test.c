4beced05cbce53c5c7f20559d698685252b5316c
Egi Yuda <egiyudasebayang@gmail.com>
2026-03-30 16:06:27 +0700

    tambah fitur kali

--- test.c
+++ test.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hallo World");
+    printf("Hallo World\n");
+
+    int a, b, hasil;
+
+    printf("=== Program perkalian sederhana ===\n");
+    printf("Input angka a : ");
+    scanf("%d", &a);
+    printf("Input angka b : ");
+    scanf("%d", &b);
+
+    hasil = a*b;
+    printf("Hasil Kali = %d", hasil);
+
 
     return 0;
 }
